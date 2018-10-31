21d244c309b379e6ae79b5ffdd05534ca6cd86f0
Shane Guittap <shaneguittap@Kristins-MacBook-Pro-2.local>
2018-10-31 15:13:25 -0600

    app up and linked

---
+++ reac-giphy/src/HelloWorld/HelloWorld.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class HelloWorld extends Component{
+    render(){
+
+    
+    return(
+        <div>
+            <h2>Hello World</h2>
+        </div>
+        )
+    }
+}
+export default HelloWorld;
