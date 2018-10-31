69f39c8d43791c725a87bb98d797d398475dadd6
Shane Guittap <shaneguittap@Kristins-MacBook-Pro-2.local>
2018-10-31 16:16:25 -0600

    step3

--- reac-giphy/src/Home.js
+++ reac-giphy/src/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './Home.css';
 import HelloWorld from './HelloWorld/HelloWorld'
+import Search from './Search/Search'
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
         <HelloWorld/>
+        <Search/>
       </div>
     );
   }
---
+++ reac-giphy/src/Search/Search.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+class Search extends Component{
+    render(){
+        return(
+            <div>
+                <input type="text" className="input" placeholder="search"/>
+            </div>
+        )
+    } 
+}
+export default Search;
