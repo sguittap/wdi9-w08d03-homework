cef726bc7547ca17665a03ba3ba09aa8907b5058
Shane Guittap <shaneguittap@Kristins-MacBook-Pro-2.local>
2018-10-31 20:26:53 -0600

    step3

--- reac-giphy/src/Search/Search.js
+++ reac-giphy/src/Search/Search.js
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
 class Search extends Component{
+    constructor(){
+        super()
+        this.state={}
+    }
+    handleChange=(e)=> {
+        
+    }
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        
+      }
     render(){
         return(
             <div>
+                <h5>search me so hard..</h5>
+                <form onSubmit={this.handleSubmit}>
                 <input type="text" className="input" placeholder="search"/>
+                <button type="Submit">Search</button>
+                </form>
             </div>
         )
     } 
